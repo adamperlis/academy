93a8af0b8e6b2adab5d294f52bb3980ead6f4a7a
Adam Perlis <adam.perlis@gmail.com>
2016-12-27 14:57:47 -0500

    got innovation services up and runnning

--- src/app/views/innovationServices/InnovationServices.js
+++ src/app/views/innovationServices/InnovationServices.js
@@ -4,6 +4,8 @@ import React, {
 }                     from 'react';
 import cx             from 'classnames';
 import shallowCompare from 'react-addons-shallow-compare';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { Link }       from 'react-router';
 
 class InnovationServices extends Component {
 
@@ -34,10 +36,100 @@ class InnovationServices extends Component {
           'animatedViews': animated,
           'view-enter': viewEntersAnim
         })}>
-        <h1>
-          Innovation  
-        </h1>
-        <h2>Services</h2>
+        <Row className="show-grid innovation hero">
+          <Col xs={8}> 
+            <h1>Product</h1>
+            <h2>Design & Development</h2>
+            
+            <Link
+              className="btn"
+              to={'#'}>
+              SEE OUR SERVICES
+            </Link>
+          </Col>
+          <Col xs={4}></Col> 
+        </Row>
+
+        <Row className="show-grid ignite-talk">
+          <Col xs={7}> 
+
+          <h3>
+          Build
+          </h3>
+          <h4>
+          Prototypes
+          </h4>
+
+          <p className="subhead">
+          We embed with your team to rapidly develop prototypes and work with your teams to innovate and break down silos of knowledge to create a fully-transparent partnership.
+          </p> 
+          <Link
+              className="btn"
+              to={'#'}>
+              Build With Us
+          </Link>
+
+          </Col>
+          <Col xs={5}></Col>
+        </Row>
+
+        <Row className="show-grid arrow">
+          <Col xs={12}> 
+            <img src={require('../../images/big-arrow-down.svg')} width="100%" className=""/> 
+          </Col>
+        </Row>
+
+        <Row className="show-grid workshop">
+          <Col xs={5}></Col> 
+          <Col xs={7}> 
+
+          <h3>
+          User
+          </h3>
+          <h4>
+          Testing
+          </h4>
+
+          <p className="subhead">
+          We spend time with your users, quickly testing hypotheses. Every decision we make to be validated with both qualitative and quantative data by running usability tests and programmed metrics.           
+          </p>
+          <Link
+              className="btn"
+              to={'#'}>
+              Test with Us          
+              </Link>
+
+          </Col>
+        </Row>
+
+        <Row className="show-grid arrow-reverse">
+          <Col xs={12}> 
+            <img src={require('../../images/big-arrow-down-reverse.svg')} width="100%" className=""/> 
+          </Col>
+        </Row>
+
+        <Row className="show-grid bootcamp">
+          <Col xs={6}> 
+
+          <h3>
+          Launch
+          </h3>
+          <h4>
+          Products
+          </h4>
+
+          <p className="subhead">
+          Agile development means that we are constanlty building and testing. Your product isn’t done once it launches, our team will ensure that critical bugs are managed, metrics are being monitored and if something isn’t working for users then we are going to fix it.
+          </p>
+           <Link
+              className="btn"
+              to={'#'}>
+              Launch with Us
+          </Link>
+
+          </Col>
+          <Col xs={6}></Col>
+        </Row>
       </div>
     );
   }
