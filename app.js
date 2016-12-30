32684e0bff876754fecd208baaed3f387638b32d
Adam Perlis <adam.perlis@gmail.com>
2016-12-30 14:20:24 -0500

    reset start script to dev mode

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('../webpack.dev.config.js')
+  const config = require('./webpack.config.babel.js')
   const compiler = webpack(config)
-
+  console.log(config.default.output.publicPath, "=======")
   app.use(webpackHotMiddleware(compiler))
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
-    publicPath: config.output.publicPath
-  }))
+    publicPath: "/"  }))
 }
 
 app.listen(port)
