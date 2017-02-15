e73f837b82d852cca42207cb545938f5e6471a09
Adam Perlis <adam.perlis@gmail.com>
2017-02-15 16:43:24 -0500

    added Google analytics

--- src/app/routes/Route.js
+++ src/app/routes/Route.js
@@ -26,14 +26,22 @@ import {
 import configureStore           from '../redux/store/configureStore';
 import DevTools                 from '../redux/devTools/DevTools.jsx';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-92024351-1');
+
 const store         = configureStore();
 const syncedHistory = syncHistoryWithStore(browserHistory, store);
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 export const Routes = () => {
   return (
     <Provider store={store}>
       <div>
-        <Router onUpdate={() => window.scrollTo(0, 0)} history={syncedHistory}>
+        <Router onUpdate={() => window.scrollTo(0, 0),logPageView} history={syncedHistory}>
           <Route path="/" component={App} >
             <IndexRoute component={ConnectedHome} />
             <Route path="/designSprint" component={ConnectedDesignSprint} />
