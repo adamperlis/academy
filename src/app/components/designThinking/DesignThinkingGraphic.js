db75f5469674247104a922a27728386b6cfeb642
Adam Perlis <adam.perlis@gmail.com>
2017-02-16 19:28:24 -0500

    56b72eef397da3b4b106362712d22064b8ce968a

--- src/app/components/designThinking/DesignThinkingGraphic.js
+++ src/app/components/designThinking/DesignThinkingGraphic.js
@@ -6,10 +6,10 @@ const DesignThinkingGraphic = (props) => {
 
 
 <svg width="307px" height="275px" viewBox="6361 6438 307 275">
-    <g className="human-centered-icon" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(6361.000000, 6439.000000)">
+    <g className="human-centered-icon" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(6361.000000, 6439.000000)">
         <circle id="Oval-5" stroke="#6D65CB" cx="159.5" cy="141.5" r="90.5585106"></circle>
         <g id="Group-11_2_" transform="translate(124.383960, 101.535591)">
-        <path id="Stroke-3506_3_" fill="#FFE2BA" stroke="#6D65CB" strokeLinejoin="round" d="M39.3,81.4h30.8c0.5-0.4,1.1-0.7,1.6-1.1
+        <path id="Stroke-3506_3_" fill="#FFE2BA" stroke="#6D65CB" stroke-linejoin="round" d="M39.3,81.4h30.8c0.5-0.4,1.1-0.7,1.6-1.1
         c-0.9-0.8-1.9-1.6-3-2.2l-15.1-8.8c-2.1-1.1-8-3.4-8-5.8V57c0,0,10.3-5.6,12.1-13.5c1.5-1,3.9-2.6,3.9-4.5v-7.2
         c0-0.6-0.3-1.2-0.8-1.8c-1.5-1.8-2.4-4-2.4-6.3v-3.3c0,0-0.5-19.2-22.5-19.2h-0.3c-21.9,0-22.5,19.2-22.5,19.2v3.3
         c0,2.3-0.9,4.5-2.4,6.3c-0.5,0.6-0.8,1.2-0.8,1.8V39c0,1.9,2.4,3.6,3.9,4.5C15.7,51.4,26.1,57,26.1,57v6.6c0,2.4-5.9,4.6-8,5.8
@@ -18,31 +18,31 @@ const DesignThinkingGraphic = (props) => {
         <g id="Group-17" transform="translate(159.500000, 141.500000) rotate(90.000000) translate(-159.500000, -141.500000) translate(46.000000, 28.000000)">
             <path d="M196.121451,28.375161 C196.121451,28.8558647 195.925723,29.3158028 195.578892,29.6489375 L184.091487,40.6945194 C183.749074,41.0241196 183.307692,41.188036 182.866753,41.188036 C182.402838,41.188036 181.939807,41.0064467 181.592976,40.6454771 C180.916103,39.9420944 180.938194,38.8233979 181.642018,38.1469664 L191.804396,28.375161 L181.642018,18.6033556 C180.938194,17.9269241 180.916103,16.8082276 181.592976,16.104845 C182.268966,15.4010205 183.387662,15.3789293 184.091487,16.0558026 L195.578892,27.1013845 C195.925723,27.4345193 196.121451,27.8944573 196.121451,28.375161" id="chevron-right" fill="#6D65CB" transform="translate(188.610421, 28.375127) rotate(45.000000) translate(-188.610421, -28.375127) "></path>
             <path d="M37.5552744,190.279573 C37.5552744,190.760276 37.3595467,191.220215 37.0127154,191.553349 L25.5253102,202.598931 C25.1828971,202.928531 24.7415157,203.092448 24.3005761,203.092448 C23.8366616,203.092448 23.3736308,202.910858 23.0267995,202.549889 C22.3499263,201.846506 22.3720174,200.72781 23.0758419,200.051378 L33.2382191,190.279573 L23.0758419,180.507767 C22.3720174,179.831336 22.3499263,178.712639 23.0267995,178.009257 C23.7027892,177.305432 24.8214857,177.283341 25.5253102,177.960214 L37.0127154,189.005796 C37.3595467,189.338931 37.5552744,189.798869 37.5552744,190.279573" id="chevron-right" fill="#6D65CB" transform="translate(30.044245, 190.279539) rotate(-125.000000) translate(-30.044245, -190.279539) "></path>
-            <circle id="Oval-4" stroke="#6D65CB" strokeDasharray="7" cx="113.5" cy="113.5" r="113.5"></circle>
+            <circle id="Oval-4" stroke="#6D65CB" stroke-dasharray="7" cx="113.5" cy="113.5" r="113.5"></circle>
         </g>
         <g id="Group-16" opacity="0.495131341">
             <circle id="Oval-6" fill="#6D65CB" cx="234.5" cy="246.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="77.5" cy="254.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-15" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="287.5" cy="193.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="77.5" cy="254.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-15" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="287.5" cy="193.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-14" fill="#6D65CB" cx="130.5" cy="271.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-13" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="207.5" cy="269.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-13" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="207.5" cy="269.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-12" fill="#6D65CB" cx="285.5" cy="130.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-11" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="292.5" cy="83.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-11" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="292.5" cy="83.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-10" fill="#6D65CB" cx="137.5" cy="19.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-9" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="99.5" cy="22.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-8" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="303.5" cy="167.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-7" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="284.5" cy="230.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-6" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="163.5" cy="264.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-5" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="109.5" cy="254.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-4" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="257.5" cy="58.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-3" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="173.5" cy="2.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-11" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="42.5" cy="216.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-16" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" fill="#6D65CB" cx="32.5" cy="88.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-9" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="99.5" cy="22.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-8" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="303.5" cy="167.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-7" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="284.5" cy="230.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-6" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="163.5" cy="264.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-5" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="109.5" cy="254.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-4" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="257.5" cy="58.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-3" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="173.5" cy="2.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-11" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="42.5" cy="216.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-16" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" fill="#6D65CB" cx="32.5" cy="88.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-10" fill="#6D65CB" cx="7.5" cy="132.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-19" fill="#6D65CB" cx="57.5" cy="30.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-18" fill="#6D65CB" cx="2.5" cy="188.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-3" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="29.5" cy="172.5" r="2.5"></circle>
-            <circle id="Oval-6-Copy-17" stroke="#6D65CB" strokeWidth="0.799999952" strokeDasharray="0.7999999523162842" cx="29.5" cy="56.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-3" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="29.5" cy="172.5" r="2.5"></circle>
+            <circle id="Oval-6-Copy-17" stroke="#6D65CB" stroke-width="0.799999952" stroke-dasharray="0.7999999523162842" cx="29.5" cy="56.5" r="2.5"></circle>
             <circle id="Oval-6-Copy-2" fill="#6D65CB" cx="219.5" cy="32.5" r="2.5"></circle>
         </g>
     </g>
--- src/app/components/topBarNav/topBarNav.js
+++ src/app/components/topBarNav/topBarNav.js
@@ -41,38 +41,28 @@ class TopBarNav extends Component {
   constructor(props){
     super(props);
     this.state = {open:false};
-    this.state = {
-      value: '/',
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-      this.handleRouteActions(nextProps.currentView.views.viewIndex);
    }
 
  // This removes the inkBar from the tab and closes the drawer. 
   handleTopNavActiveClass = () => {
-    this.setState({currentView: -1});
     this.state.open !== false ? this.handleClose() : false;
    };
 
   handleToggle() {
     this.setState({open: !this.state.open});
-
   }
 
   handleRouteActions = (value) => {
-
-    this.setState({currentView: value});
     this.state.open !== false ? this.handleClose() : false;
-  }
+ }
 
-  handleClose() { this.setState({open: false}); }
-        render() {           
+  handleClose() { this.setState({open: false}); console.log("closed"); }
+
+        render() {     
             return (
                 <div>
                 <Drawer
-                  containerStyle={{background: '#6d65cb',}}
+                  containerStyle={{background: '#6d65cb'}}
                   docked={false}
                   openSecondary={false}
                   open={this.state.open}
@@ -86,19 +76,19 @@ class TopBarNav extends Component {
                     Home
                     </Link>
                   </MenuItem>
-                  <Divider style={{background:'rgb(126, 119, 204)',}}/>
+                  <Divider style={{background:'rgb(126, 119, 204)'}}/>
                   <MenuItem>
                     <Link
                     onTouchTap={this.handleTopNavActiveClass.bind(this)}
                     className="drawer"
                     to={'#'}>
-                    What's Design Thinking?
+                    What is Design Thinking?
                     </Link>
                   </MenuItem>
-                  <Divider style={{background:'rgb(126, 119, 204)',}}/>
+                  <Divider style={{background:'rgb(126, 119, 204)'}}/>
                   <MenuItem>
                     <Link
-                    onTouchTap={this.handleRouteActions.bind(this, 1)} 
+                    onTouchTap={this.handleRouteActions.bind(this, 'designSprint')} 
                     className="drawer"
                     to={'/designSprint'}>
                     Sprint Academy
@@ -107,7 +97,7 @@ class TopBarNav extends Component {
                   <Divider style={{background:'rgb(126, 119, 204)',}}/>
                   <MenuItem>
                     <Link
-                    onTouchTap={this.handleRouteActions.bind(this, 2)}
+                    onTouchTap={this.handleRouteActions.bind(this, 'ProductDesign')}
                     className="drawer"
                     to={'/ProductDesign'}>
                     Design & Development
@@ -116,12 +106,21 @@ class TopBarNav extends Component {
                   <Divider style={{background:'rgb(126, 119, 204)',}}/>
                   <MenuItem>
                     <Link
+                    onTouchTap={this.handleRouteActions.bind(this, 'Work')}
+                    className="drawer"
+                    to={'/Work'}>
+                    Work
+                    </Link>
+                  </MenuItem>
+                  {/*<Divider style={{background:'rgb(126, 119, 204)',}}/>
+                  <MenuItem>
+                    <Link
                     onTouchTap={this.handleRouteActions.bind(this, 3)}
                     className="drawer"
                     to={'/InnovationServices'}>
                     Innovation Services
                     </Link>
-                  </MenuItem>
+                  </MenuItem>*/}
                   <Divider style={{background:'rgb(126, 119, 204)',}}/>
                   <MenuItem>
                     <Link
@@ -143,22 +142,20 @@ class TopBarNav extends Component {
                       borderBottom:'1px solid #6D65CB',
                     }} 
                     titleStyle={{fill:'rgb(0, 0, 0)',}}
-                    showMenuIconButton={true} 
-                    onTitleTouchTap={this.handleToggle.bind(this)}
                     title={
                       <Link
                       className="nav-logo"
                       onTouchTap={this.handleTopNavActiveClass.bind(this)}
                        to={'/'}>
-                      <img src={'./public/assets/images/academy-logo-black.svg'} height="24px"/>
+                      <img src='/public/assets/images/academy-logo-black.svg' height="24px"/>
                       </Link>
-                      }
+                    }
                     onTouchTap={this.handleToggle.bind(this)}
                     iconElementRight={
                       <div>
                       <FlatButton className="navLinks hidden-xs hidden-sm" label="Sprint Academy" containerElement={<Link  to="/designSprint"/>} />
                       <FlatButton className="navLinks hidden-xs hidden-sm" label="Design & Development" containerElement={<Link  to="/ProductDesign"/>} />
-                      <FlatButton className="navLinks hidden-xs hidden-sm" label="Innovation Services" containerElement={<Link  to="/InnovationServices"/>} />
+                      <FlatButton className="navLinks hidden-xs hidden-sm" label="Work" containerElement={<Link  to="/Work"/>} />
                       </div>
                       }
                     iconStyleRight={{
@@ -167,11 +164,11 @@ class TopBarNav extends Component {
                     />
 
                   
-                  <Tabs value={this.state.currentView || -1}  onChange={this.handleRouteActions} className="small-nav hidden-md hidden-lg">
+                  <Tabs value={this.props.currentView.views.currentView}  onChange={this.handleRouteActions} className="small-nav hidden-md hidden-lg">
 
-                    <Tab value={1} style={styles.headline} label="Sprint Academy" containerElement={<Link  to="/designSprint"/>} />
-                    <Tab value={2} style={styles.headline} label="Design & Development" containerElement={<Link to="/ProductDesign"/>} />
-                    <Tab value={3} style={styles.headline} label="Innovation Services" containerElement={<Link to="/InnovationServices"/>} />
+                    <Tab value={'designSprint'} style={styles.headline} label="Sprint Academy" containerElement={<Link to="/designSprint"/>} />
+                    <Tab value={'ProductDesign'} style={styles.headline} label="Design & Development" containerElement={<Link to="/ProductDesign"/>} />
+                    <Tab value={'Work'} style={styles.headline} label="Work" containerElement={<Link to="/Work"/>} />
                 
                   </Tabs>
                   </Sticky>
